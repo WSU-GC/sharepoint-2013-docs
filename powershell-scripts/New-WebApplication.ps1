67df3ed71a46af5633ff61a848ce8a0b939b4145
dworthen <worthend.derek@gmail.com>
2015-07-01 19:57:27 -0700

    Updates

---
+++ powershell-scripts/New-WebApplication.ps1
@@ -0,0 +1,32 @@
+﻿param(
+    [Parameter(Mandatory=$true, HelpMessage="IIS Name & Central Admin Display Name")][String] $Name,
+    [Parameter(Mandatory=$true, HelpMessage="url including https:// protocol")][String] $Url,
+    [Parameter(Mandatory=$true, HelpMessage="domain\account")][String] $account
+) 
+
+echo on
+
+$HostHeader = Read-Host("Enter host header(IIS Server Name Indication). Should be left blank for the App Domain Web App")
+
+$trimmedName = $Name.Replace(" ","_").Replace("-", "_")
+$AppPool = "WebAppl_" + $trimmedName
+$DB = "WebApp_" + $trimmedName
+
+$ap = New-SPAuthenticationProvider
+
+if($hostheaders.Length -ne 0) {
+    New-SPWebApplication -Name $Name -URL $Url -Port 443 `
+    -ApplicationPool $AppPool ` 
+    -ApplicationPoolAccount (Get-SPManagedAccount $account) `
+    -AuthenticationProvider $ap -SecureSocketsLayer `
+    -DatabaseName $DB `
+    -HostHeader $HostHeader
+
+} else {
+    New-SPWebApplication -Name $Name -URL $Url -Port 443 `
+    -ApplicationPool $AppPool ` 
+    -ApplicationPoolAccount (Get-SPManagedAccount $account) `
+    -AuthenticationProvider $ap -SecureSocketsLayer `
+    -DatabaseName $DB
+}
+
