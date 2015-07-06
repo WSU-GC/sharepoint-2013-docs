b96cc2be085129b40f001f1990db9e9ab26bc719
unknown <dworthen@GC-SharePoint-Dev.ad.wsu.edu>
2015-07-06 14:54:13 -0700

    added app deployment scripts

---
+++ powershell-scripts/install-certs.ps1
@@ -0,0 +1,4 @@
+﻿foreach ($certPath in $args) {
+ $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certPath)
+ New-SPTrustedRootAuthority -Name $certPath -Certificate $cert
+}
